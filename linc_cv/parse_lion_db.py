5b2d900cbc28f9d507548a704fc22569eeb2eda2
Adam Gradzki <contact@adamgradzki.com>
2018-07-27 08:53:34 -0600

    Parse LINC lion DB from LINC API

--- linc_cv/parse_lion_db.py
+++ linc_cv/parse_lion_db.py
@@ -1,18 +1,60 @@
 import json
 from collections import defaultdict
+from io import BytesIO
+from json import dumps
+from urllib.request import urlretrieve as download_it
+from zipfile import ZipFile
 
-from . import IMAGES_LUT_PATH
+from requests import get, post
+
+from . import IMAGES_LUT_PATH, ClassifierError
+from .keys import LINC_USERNAME, LINC_PASSWORD
 
 
-def parse_lion_database(db_path):
+def download_lion_db():
+    body = {'username': LINC_USERNAME, 'password': LINC_PASSWORD}
+    headers = {'Content-Type': 'application/json'}
+    urllogin = 'https://linc-api.herokuapp.com/auth/login'
+    urldbdump = 'https://linc-api.herokuapp.com/lions/'
+    resp = post(urllogin, data=dumps(body), headers=headers)
+    if resp.status_code == 200:
+        print('Authentication success!')
+        headers['Linc-Api-AuthToken'] = resp.json()['data']['token']
+        respdb = get(urldbdump, headers=headers)
+        if respdb.status_code == 403:
+            raise ClassifierError('Your are not allowed to access the database dump file.')
+        elif respdb.status_code == 404:
+            raise ClassifierError('Database dump file not found. It\'s being created. Try again soon.')
+        elif respdb.status_code in [200, 201]:
+            url = respdb.json()['data']['url']
+            fname = url.split('/')[-1]
+            print(f'fname: {fname}')
+            from tempfile import NamedTemporaryFile
+            with NamedTemporaryFile(suffix='.zip') as ntf:
+                download_it(url, ntf.name)
+                ntf.seek(0)
+                zf = ZipFile(ntf)
+                return json.load(BytesIO(zf.read(zf.filelist[0])))
+        else:
+            print('Fail to request the dump')
+    else:
+        print('Authentication failure')
+
+
+def parse_lion_database(*, db_json_path=None, download_db_zip=False):
     """
     Parse LINC database into a lookup table consisting of lion_ids
     and image URLs for each feature type for each lion. Feature types
     include whiskers and 'cv' images
     """
 
-    with open(db_path) as f:
-        lions = json.load(f)
+    if db_json_path:
+        with open(db_json_path) as f:
+            lions = json.load(f)
+    elif download_db_zip:
+        lions = download_lion_db()
+    else:
+        raise ClassifierError('database path or url not specified')
 
     lion_db = defaultdict(lambda: defaultdict(list))
     for lion in lions['data']:
