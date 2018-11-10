e2bf390594f841d44fecb3aed3baa42f6b674bec
Adam Gradzki <contact@adamgradzki.com>
2018-11-10 11:30:19 -1000

    Log each YOLOv3 detection event

--- linc_cv/modality_whisker/inference.py
+++ linc_cv/modality_whisker/inference.py
@@ -101,6 +101,7 @@ class YOLO(object):
         image_data = np.array(boxed_image, dtype='float32')
 
         print(image_data.shape)
+        print('running YOLOv3 detect_image...')
         image_data /= 255.
         image_data = np.expand_dims(image_data, 0)  # Add batch dimension.
 
