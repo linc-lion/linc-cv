41a57bd2380ebad4e2af0870ed04f12cad223990
Adam Gradzki <hi@adamgradzki.com>
2021-06-30 23:51:25 +0000

    Remove unneeded complexity

--- update-dev.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-clear && rsync -rtvz --exclude data --exclude .git \
-    --exclude .pytest* --exclude .idea --exclude \*.egg\* --exclude __pycache__  \
-    . cassiopeia:linc-cv
-
--- update-prod.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-clear && rsync -rtvz --delete \
-    --exclude data --exclude .git \
-    --exclude .pytest* --exclude .idea \
-    --exclude \*.egg\* --exclude __pycache__  \
-    ~/customers/linc/linc-cv-server/ linc:linc-cv-server
-
