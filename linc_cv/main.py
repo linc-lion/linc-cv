fc299202b9dea59c1890e9bbd1081c9d12e33992
shihgianlee <shih.lee@gmail.com>
2021-10-02 10:12:30 -0700

    The supervisor options have been moved to supervisors configuration files for clarity. This decouple the need for supervisor to rely on linc_cv package.

--- linc_cv/main.py
+++ linc_cv/main.py
@@ -1,18 +1,10 @@
 import argparse
-import os
-import sys
-from subprocess import run
 
-from linc_cv.settings import BASE_DIR
-from linc_cv.parse_lion_db import parse_lion_database
-from linc_cv.web import app
-from linc_cv.modality_whisker.download import download_whisker_images
-from linc_cv.modality_whisker.train import train_whisker_classifier
 from linc_cv.modality_cv.download import download_cv_images
 from linc_cv.modality_cv.train import extract_cv_features, train_cv_classifier
-
-CELERY_EXE_PATH = os.path.join(os.path.dirname(sys.executable), 'celery')
-FLOWER_EXE_PATH = os.path.join(os.path.dirname(sys.executable), 'flower')
+from linc_cv.modality_whisker.download import download_whisker_images
+from linc_cv.modality_whisker.train import train_whisker_classifier
+from linc_cv.parse_lion_db import parse_lion_database
 
 
 def main():
@@ -44,19 +36,6 @@ def main():
 
     # </ whisker specific >
 
-    parser.add_argument(
-        '--web', action='store_true',
-        help="Start HTTP REST API")
-    parser.add_argument(
-        '--worker-training', action='store_true',
-        help="Training queue worker")
-    parser.add_argument(
-        '--worker-classification', action='store_true',
-        help="Classification queue worker")
-    parser.add_argument(
-        '--flower', action='store_true',
-        help="Start API task worker monitor (Celery Flower)")
-
     args = parser.parse_args()
 
     if args.parse_lion_database:
@@ -84,23 +63,6 @@ def main():
         train_whisker_classifier()
 
     # </ whisker specific >
-
-    if args.web:
-        app.run(host='0.0.0.0', port=5000, debug=False)
-
-    if args.worker_training:
-        cmd = f'{CELERY_EXE_PATH} worker -A linc_cv.tasks --concurrency 1 ' \
-            f'-Q training -E -n training@%h'.split(' ')
-        run(cmd, check=True, cwd=BASE_DIR)
-
-    if args.worker_classification:
-        cmd = f'{CELERY_EXE_PATH} worker -A linc_cv.tasks --concurrency 1 ' \
-            f'-Q classification -E -n classification@%h'.split(' ')
-        run(cmd, check=True, cwd=BASE_DIR)
-
-    if args.flower:
-        cmd = f'{FLOWER_EXE_PATH} flower -A linc_cv.tasks --address=0.0.0.0 --port=5555'.split(' ')
-        run(cmd, check=True, cwd=BASE_DIR)
 
 
 if __name__ == '__main__':
