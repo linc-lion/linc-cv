0e6788fee71401874de28f86affc1a7255154df1
Habiba Mohamed <habibam314@gmail.com>
2023-06-19 09:52:57 +0300

    add user data script

--- .github/workflows/main.tf
+++ .github/workflows/main.tf
@@ -11,3 +11,15 @@ resource "aws_instance" "ec2_instance" {
     Name = "MyEC2Instance"
   }
 }
+
+  user_data = <<-EOF
+    #!/bin/bash
+    ssh -i ~/my_pem.pem ubuntu@current_instance_ip
+    mv /home/ubuntu/linc-cv /home/ubuntu/linc-cv-backup
+    aws s3 cp s3://linc-backup/linc-cv/linc-cv-20181223.tar.gz /home/ubuntu/linc-cv-backup/linc-cv-20181223.tar.gz
+    sudo supervisorctl stop all
+    sudo supervisorctl status
+    scp -r /path/to/updated/linc-cv ubuntu@current_instance_ip:/home/ubuntu/
+    sudo supervisorctl start all
+    sudo supervisorctl status
+  EOF
